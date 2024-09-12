390c3814b8949e8867f5a6c25b4b4004ecafafac
umedsingh98 <umedsingh0898@gmail.com>
2024-09-12 15:13:44 +0530

    db changes

--- Backend/db.js
+++ Backend/db.js
@@ -5,10 +5,7 @@ dotenv.config(); // Load environment variables from .env
 
 const connectToMongo = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected To Database Successfully...");
   } catch (error) {
     console.error("Error connecting to the database:", error);
